ffef2536b07510122c70efec426b66ce1bc8efcb
triadmusic <dhill@triadmusic.net>
2025-05-05 13:57:06 -0400

    Update AppDelegate.swift

--- common/common/AppDelegate.swift
+++ common/common/AppDelegate.swift
@@ -3,7 +3,7 @@ import BNBSdkApi
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var banubaClientToken: String = <#Place your token here#>
+    var banubaClientToken: String = <"Qk5CIJZiH1DWWC/D8oNCoNZUMLzA+xiwV86Ssw282u/xUzcuaYzzNVE39hAAsU0U3HIQQWnGtTdKIjE9PnCFDkoV3qv/BKUw7eAX4/c5WNsgtVJT82zhsOYE9mEMEYauizCp+PfTImbrHQNYZwVKCh/7MQWUbyYYsAWTo3EcH9922Gl29rMh4tKuaukt6nB82ohZETyB4hGzV4JjnReonp9Fs9K/fEKyNoOSbTzmPTLFHAaPZ/oyjMi6aSQYnu5fLumoNd7JFaR00nRUCkrqavFSu01V7DkjHLoRv5RerN/Y03W3zekhuP3mgLURn5AOHaYwu0swQyANlCfByRZQarBOFerY9GTPhkEYQmSrGNZNY+tU1cMCaGZw/38BeDDjSzHd4FUhdqL3VjIl4z/Vrg3F6mhYTkJxnqtHaz9ijSe3sOF9ARmYgv8lBTewQTEnDfeXtiAd7cO+LIV0B+tsxgKg/Dn1VAp/NK0GnwQ+bdVUKRlWmK5ppn15uQIQgQ9QIF/w9hwEatLxdkR8uSVNAtCPqwm1YOugS3oqC2Ky4tFeXS+PCLQrOZj22OlZullNj6dZNP78FlfkUTt56Q04i+UbJoyqE2JuLESXIvLv6304u9PVXzEK/Qw21xoz1NJ7tVVD17O4IQXHcNF1lSPzSQ==">
 
     var window: UIWindow?
 
@@ -22,4 +22,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 }
-
